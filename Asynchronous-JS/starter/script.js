67120b3709b2a43eaa71cd9174715f3a4a7ba3b3
vk196 <victorumeh196@gmail.com>
2023-06-03 08:36:12 -0700

    first Async/API/promises lesson

---
+++ Asynchronous-JS/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+//Snippet to understand Async concept
+const asyHeadTest = document.querySelector('h1');
+setTimeout(() => (asyHeadTest.style.opacity = 1), 5000);
+alert('Hello World');
+///////////////////////////////////////
